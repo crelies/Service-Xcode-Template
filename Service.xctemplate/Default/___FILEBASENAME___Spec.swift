7e09b36e1c95e84750f071d0bfc8f910fa660a98
Elies, Christian <christian.elies@immonet.de>
2018-06-21 10:26:29 +0200

    feat(): added unit test file; fix(): file name is inherited now

---
+++ Service.xctemplate/Default/___FILEBASENAME___Spec.swift
@@ -0,0 +1,16 @@
+@testable import ___PROJECTNAME___
+import Nimble
+import Quick
+import UIKit
+
+final class ___VARIABLE_SERVICENAME___Spec: QuickSpec {
+	override func spec() {
+		describe("___VARIABLE_SERVICENAME___") {
+			context("when ...") {
+				it("should ...") {
+
+				}
+			}
+		}
+	}
+}
